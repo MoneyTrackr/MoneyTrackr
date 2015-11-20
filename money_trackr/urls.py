18cdbe68319d91478f95a0899fa7a0cee17f50cd
Shirl <shirl_mabeza@yahoo.com>
2015-11-20 11:00:18 +0800

    add bootstrap and configure base.html

--- money_trackr/urls.py
+++ money_trackr/urls.py
@@ -26,3 +26,10 @@ urlpatterns = patterns('',
     url(r'^expense/\d+/edit$', expense_edit),
     url(r'^$', home),
 )
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = urlpatterns + [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

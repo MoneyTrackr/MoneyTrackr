886f4c8025698cce7d1e783eb091bb718e79413a
Shirl <shirl_mabeza@yahoo.com>
2015-11-21 11:18:00 +0800

    update income_test.py: remove unnecessasry comments

--- tests/income_test.py
+++ tests/income_test.py
@@ -2,21 +2,21 @@
 from selenium import webdriver
 import unittest
 
-class NewVisitorTest(unittest.TestCase):  #1
+class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self):  #2
+    def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    def tearDown(self):  #3
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_display_the_income_list(self):  #4
+    def test_can_display_the_income_list(self):
         self.browser.get('http://localhost:8000/income')
 
-        self.assertIn('Income List', self.browser.title)  #5
-        self.fail('Finish the test!')  #6
+        self.assertIn('Income List', self.browser.title)
+        self.fail('Finish the test!')
 
 
-if __name__ == '__main__':  #7
-    unittest.main(warnings='ignore')  #8
+if __name__ == '__main__': 
+    unittest.main(warnings='ignore')

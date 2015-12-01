f31e5fa27876050f44a84c077923fb1c72b87c28
godimanarig <godimanarig@up.edu.ph>
2015-12-01 08:49:11 +0800

    fixed displayof text values in category and account dropdown menu. yehey!

--- application/models.py
+++ application/models.py
@@ -7,8 +7,14 @@ class Category(models.Model):
     name = models.CharField(max_length=30)
     type = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 class Account(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 class Income(models.Model):
     date = models.DateField()
@@ -21,7 +27,7 @@ class Income(models.Model):
     # cat_data = serializers.serialize('python', category_list, fields=('id','name'))
     # acc_data = serializers.serialize('python', account_list, fields=('id','name'))
     # category = models.CharField(max_length=1, choices=cat_data)
-    
+
 
 class Expense(models.Model):
     date = models.DateField()

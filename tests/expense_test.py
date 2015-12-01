1b94e81643b35ee1e1da2bf7997e2e4a0e8946a7
Shirl <shirl_mabeza@yahoo.com>
2015-12-01 22:43:29 +0800

    update test cases and expense/new.html

--- tests/expense_test.py
+++ tests/expense_test.py
@@ -24,60 +24,59 @@ class ExpenseTest(StaticLiveServerTestCase):
         expense = Expense(category=category, account=account, notes="gift", amount=1000, date=datetime.datetime.now())
         expense.save()
 
-    # def tearDown(self):
-    #     # self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-    # def test_can_display_the_expense_list(self):
-    #
-    #     self.browser.get('http://localhost:8081/expense')
-    #
-    #     self.assertIn('Expense List', self.browser.title)
-    #     self.assertIn('gift', self.browser.page_source)
-    #     self.assertIn('savings', self.browser.page_source)
-    #     self.assertIn('others', self.browser.page_source)
-    #     self.assertIn('1000', self.browser.page_source)
-    #
-    #
-    # def test_can_visit_add_expense_page(self):
-    #     self.browser.get('http://localhost:8081/expense')
-    #     add_expense_link = self.browser.find_element_by_link_text('Add Expense')
-    #     add_expense_link.click()
-    #     self.browser.implicitly_wait(10)
-    #
-    #     self.assertIn('New Expense', self.browser.title)
-    #     self.assertIn('This is my new expense page.', self.browser.page_source)
-    #     self.fail('Finish the test!')
-    #
-    #
-    # def test_can_delete_an_epxense_item(self):
-    #     expense = Expense.objects.latest('id')
-    #     self.browser.get('http://localhost:8081/expense')
-    #     delete_expense_link = self.browser.find_element_by_id("delete-expense-"+ str(expense.id))
-    #     delete_expense_link.click()
-    #
-    #
-    #     alert = self.browser.switch_to.alert
-    #     alert.accept()
-    #     self.browser.implicitly_wait(3)
-    #     self.assertNotIn('gift', self.browser.page_source)
-    #     self.assertNotIn('savings', self.browser.page_source)
-    #     self.assertNotIn('others', self.browser.page_source)
-    #     self.assertNotIn('1000', self.browser.page_source)
-    #
-    # def test_cancels_delete_of_an_epxense_item(self):
-    #     expense = Expense.objects.latest('id')
-    #     self.browser.get('http://localhost:8081/expense')
-    #     delete_expense_link = self.browser.find_element_by_id("delete-expense-"+ str(expense.id))
-    #     delete_expense_link.click()
-    #
-    #
-    #     alert = self.browser.switch_to.alert
-    #     alert.dismiss()
-    #     self.browser.implicitly_wait(20)
-    #     self.assertIn('gift', self.browser.page_source)
-    #     self.assertIn('savings', self.browser.page_source)
-    #     self.assertIn('others', self.browser.page_source)
-    #     self.assertIn('1000', self.browser.page_source)
+    def test_can_display_the_expense_list(self):
+
+        self.browser.get('http://localhost:8081/expense')
+
+        self.assertIn('Expense List', self.browser.title)
+        self.assertIn('gift', self.browser.page_source)
+        self.assertIn('savings', self.browser.page_source)
+        self.assertIn('others', self.browser.page_source)
+        self.assertIn('1000', self.browser.page_source)
+
+
+    def test_can_visit_add_expense_page(self):
+        self.browser.get('http://localhost:8081/expense')
+        add_expense_link = self.browser.find_element_by_link_text('Add Expense')
+        add_expense_link.click()
+        self.browser.implicitly_wait(10)
+
+        self.assertIn('New Expense', self.browser.title)
+        self.assertIn('Add Expense Form', self.browser.page_source)
+
+
+    def test_can_delete_an_epxense_item(self):
+        expense = Expense.objects.latest('id')
+        self.browser.get('http://localhost:8081/expense')
+        delete_expense_link = self.browser.find_element_by_id("delete-expense-"+ str(expense.id))
+        delete_expense_link.click()
+
+
+        alert = self.browser.switch_to.alert
+        alert.accept()
+        self.browser.implicitly_wait(3)
+        self.assertNotIn('gift', self.browser.page_source)
+        self.assertNotIn('savings', self.browser.page_source)
+        self.assertNotIn('others', self.browser.page_source)
+        self.assertNotIn('1000', self.browser.page_source)
+
+    def test_cancels_delete_of_an_epxense_item(self):
+        expense = Expense.objects.latest('id')
+        self.browser.get('http://localhost:8081/expense')
+        delete_expense_link = self.browser.find_element_by_id("delete-expense-"+ str(expense.id))
+        delete_expense_link.click()
+
+
+        alert = self.browser.switch_to.alert
+        alert.dismiss()
+        self.browser.implicitly_wait(20)
+        self.assertIn('gift', self.browser.page_source)
+        self.assertIn('savings', self.browser.page_source)
+        self.assertIn('others', self.browser.page_source)
+        self.assertIn('1000', self.browser.page_source)
 
     def test_can_add_income_successfully(self):
         self.browser.get('http://localhost:8081/expense/new')

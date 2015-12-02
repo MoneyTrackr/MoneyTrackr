c4470e7f5dcc1736864dd5cb1db53af3ad0d0cb3
Shirl <shirl_mabeza@yahoo.com>
2015-12-02 13:12:37 +0800

    add test to expense

--- tests/expense_test.py
+++ tests/expense_test.py
@@ -27,6 +27,7 @@ class ExpenseTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # LIST FEATURE
     def test_can_display_the_expense_list(self):
 
         self.browser.get('http://localhost:8081/expense')
@@ -37,16 +38,7 @@ class ExpenseTest(StaticLiveServerTestCase):
         self.assertIn('others', self.browser.page_source)
         self.assertIn('1000', self.browser.page_source)
 
-
-    def test_can_visit_add_expense_page(self):
-        self.browser.get('http://localhost:8081/expense')
-        add_expense_link = self.browser.find_element_by_link_text('Add Expense')
-        add_expense_link.click()
-        self.browser.implicitly_wait(10)
-
-        self.assertIn('New Expense', self.browser.title)
-        self.assertIn('Add Expense Form', self.browser.page_source)
-
+    ## DELETE FEATURE
 
     def test_can_delete_an_expense_item(self):
         expense = Expense.objects.latest('id')
@@ -78,6 +70,17 @@ class ExpenseTest(StaticLiveServerTestCase):
         self.assertIn('others', self.browser.page_source)
         self.assertIn('1000', self.browser.page_source)
 
+    ## ADD FEATURE
+
+    def test_can_visit_add_expense_page(self):
+        self.browser.get('http://localhost:8081/expense')
+        add_expense_link = self.browser.find_element_by_link_text('Add Expense')
+        add_expense_link.click()
+        self.browser.implicitly_wait(10)
+
+        self.assertIn('New Expense', self.browser.title)
+        self.assertIn('Add Expense Form', self.browser.page_source)
+
     def test_can_add_expense_successfully(self):
         self.browser.get('http://localhost:8081/expense/new')
         input_date =  self.browser.find_element_by_id('id_date')
@@ -98,8 +101,7 @@ class ExpenseTest(StaticLiveServerTestCase):
         add_expense_button = self.browser.find_element_by_id('id_submit_expense')
         add_expense_button.click()
 
-        self.assertIn('Expense List', self.browser.title)
-
+        self.browser.implicitly_wait(10)
         self.assertIn('Expense List', self.browser.title)
         self.assertIn('family treat', self.browser.page_source)
         self.assertIn('savings', self.browser.page_source)
@@ -128,6 +130,90 @@ class ExpenseTest(StaticLiveServerTestCase):
 
         self.assertIn('Expense List', self.browser.title)
         self.assertNotIn('family treat', self.browser.page_source)
+
+    def test_detects_date_is_invalid_on_add(self):
+        self.browser.get('http://localhost:8081/expense/new')
+
+        input_category =  self.browser.find_element_by_id('id_category')
+        input_category.send_keys('others')
+
+        input_amount =  self.browser.find_element_by_id('id_amount')
+        input_amount.send_keys('100')
+
+        input_account =  self.browser.find_element_by_id('id_account')
+        input_account.send_keys('savings')
+
+        input_notes =  self.browser.find_element_by_id('id_notes')
+        input_notes.send_keys('family treat')
+
+        add_expense_button = self.browser.find_element_by_id('id_submit_expense')
+        add_expense_button.click()
+
+        self.browser.implicitly_wait(10)
+        self.assertIn('This field is required', self.browser.page_source)
+
+
+    def test_detects_category_is_invalid_on_add(self):
+        self.browser.get('http://localhost:8081/expense/new')
+        input_date =  self.browser.find_element_by_id('id_date')
+        input_date.send_keys('12/25/2015')
+
+        input_amount =  self.browser.find_element_by_id('id_amount')
+        input_amount.send_keys('100')
+
+        input_account =  self.browser.find_element_by_id('id_account')
+        input_account.send_keys('savings')
+
+        input_notes =  self.browser.find_element_by_id('id_notes')
+        input_notes.send_keys('family treat')
+
+        add_expense_button = self.browser.find_element_by_id('id_submit_expense')
+        add_expense_button.click()
+
+        self.browser.implicitly_wait(10)
+        self.assertIn('This field is required', self.browser.page_source)
+
+    def test_detects_amount_is_invalid_on_add(self):
+        self.browser.get('http://localhost:8081/expense/new')
+        input_date =  self.browser.find_element_by_id('id_date')
+        input_date.send_keys('12/25/2015')
+
+        input_category =  self.browser.find_element_by_id('id_category')
+        input_category.send_keys('others')
+
+        input_account =  self.browser.find_element_by_id('id_account')
+        input_account.send_keys('savings')
+
+        input_notes =  self.browser.find_element_by_id('id_notes')
+        input_notes.send_keys('family treat')
+
+        add_expense_button = self.browser.find_element_by_id('id_submit_expense')
+        add_expense_button.click()
+
+        self.browser.implicitly_wait(10)
+        self.assertIn('This field is required', self.browser.page_source)
+
+    def test_detects_account_is_invalid_on_add(self):
+        self.browser.get('http://localhost:8081/expense/new')
+        input_date =  self.browser.find_element_by_id('id_date')
+        input_date.send_keys('12/25/2015')
+
+        input_category =  self.browser.find_element_by_id('id_category')
+        input_category.send_keys('others')
+
+        input_amount =  self.browser.find_element_by_id('id_amount')
+        input_amount.send_keys('100')
+
+        input_notes =  self.browser.find_element_by_id('id_notes')
+        input_notes.send_keys('family treat')
+
+        add_expense_button = self.browser.find_element_by_id('id_submit_expense')
+        add_expense_button.click()
+
+        self.browser.implicitly_wait(10)
+        self.assertIn('This field is required', self.browser.page_source)
+
+    ###EDIT FEATURE
 
     def test_can_visit_edit_expense_page_(self):
         expense = Expense.objects.latest('id')
@@ -225,35 +311,3 @@ class ExpenseTest(StaticLiveServerTestCase):
         self.check_for_row_in_list_table('family treat')
                 
 
-
-
-    
-
-
-   # def test_add_expense_valid_data(self):
-   #    form = NewExpenseForm(
-   #          'date': "2015-12-01",
-   #          'category': "food",
-   #          'amount': "100.0",
-   #          'account': "savings",
-   #          'notes': "none",
-   #  }, entry=self.entry)
-   #  self.assertTrue(form.is_valid())
-   #  Expense = form.save()
-   #  self.assertEqual(Expense.date, "2015-12-01")
-   #  self.assertEqual(Expense.category, "food")
-   #  self.assertEqual(Expense.amount, "100.0")
-   #  self.assertEqual(Expense.account, "savings")
-   #  self.assertEqual(Expense.notes, "none")
-   #  self.assertEqual(Expense.entry, self.entry)
-   #
-   #  def test_add_expense_blank_data(self):
-   #      form = NewExpenseForm({}, entry=self.entry)
-   #      self.assertFalse(form.is_valid())
-   #      self.assettEqual(form.errors,{
-   #          'date': ['required']',
-   #          'category': ['required']',
-   #          'amount': ['required']',
-   #          'account': ['required']',
-   #          'notes': ['required']',
-   #      })
